3793bb7d70e6448246d257f9a0e749870b8a48fd
Eugene <evgdol93@gmail.com>
2020-07-30 01:00:14 +0300

    [-] tail

--- soft/tail/bootloader/flash.cmd
+++
@@ -1,2 +0,0 @@
-ST-LINK_CLI.exe -P "build/boot.bin" 0x08000000 -Rst
-REM ST-LINK_CLI.exe -P "base/build/fs_head.bin" 0x08000000
--- soft/tail/src/fan.h
+++
@@ -1,9 +0,0 @@
-#ifndef FAN_H
-#define FAN_H
-
-#include <stdbool.h>
-#include <stdint.h>
-
-bool fan_set(uint8_t fan, uint32_t value);
-
-#endif // FAN_H
